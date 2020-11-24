e28866ed4cd4a4240d6fb395672b8f028079d354
Celiamf <celiamartinfern@gmail.com>
2020-11-24 13:46:44 +0100

    Update docs

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -2,9 +2,8 @@ import "../stylesheets/_CharacterList.scss";
 import CharacterCard from "./CharacterCard";
 
 const CharacterList = (props) => {
-  console.log(props, "props de chrList");
   if (props.chars.length === 0) {
-    return <p>Sorry, the character you typed was not found.</p>;
+    return <p>Sorry, your search did not match any character in our list.</p>;
   } else {
     const charItems = props.chars.map((char) => {
       return (
